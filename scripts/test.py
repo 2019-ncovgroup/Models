b64cd21a97bcfd7b45c7e9c8eba97d45b75b4e53
Yadu Nand Babuji <yadunand@thetalogin6.tmi.alcf.anl.gov>
2020-03-21 02:40:41 +0000

    Adding production runner code for theta

---
+++ scripts/test.py
@@ -0,0 +1,109 @@
+# from candle_apps.candle_apps import run_intranode
+# from candle_apps.candle_apps import ModelInferer
+#from candle_apps.candle_node_local import run_local
+#import pandas as pd
+import os
+import glob
+import argparse
+from covid_screen.reg_go_infer import reg_go_infer
+import parsl
+import pickle
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--version",
+                        help="Print Endpoint version information")
+    parser.add_argument("-d", "--debug", action='store_true',
+                        help="Enables debug logging")
+    parser.add_argument("-n", "--num_files", default=10000,
+                        help="Number of files to load and run. Default=all, if set to 0 the entire file will be used")
+    parser.add_argument("-s", "--smile_dir", default=".",
+                        help="File path to the smiles csv file")
+    parser.add_argument("-b", "--batch_size", default="4",
+                        help="Size of the batch of smiles to send to each node for processing. Default=4, should be 10K")
+    parser.add_argument("-o", "--outdir", default="outputs",
+                        help="Output directory. Default : outputs")
+    parser.add_argument("-c", "--config", default="local",
+                        help="Parsl config defining the target compute resource to use. Default: local")
+    args = parser.parse_args()
+
+    #for smile_dir in glob.glob(args.smile_dir):
+    #    print(smile_dir)
+    #exit(0)
+    print(f"Loading pkl files from {args.smile_dir}")
+
+    if args.config == "local":
+        from parsl.configs.htex_local import config
+        from parsl.configs.htex_local import config
+        config.executors[0].label = "Foo"
+        config.executors[0].max_workers = 1
+    elif args.config == "theta":
+        from theta import config
+    elif args.config == "comet":
+        from comet import config
+
+    # Most of the app that hit the timeout will complete if retried.
+    # but for this demo, I'm not setting retries.
+    # config.retries = 2
+    parsl.load(config)
+
+    parsl_runner = parsl.python_app(reg_go_infer)
+
+    if args.debug:
+        parsl.set_stream_logger()
+
+
+    os.makedirs(args.outdir, exist_ok=True)
+
+    all_smile_dirs = glob.glob(args.smile_dir)
+    counter = 0
+    batch_futures = {}
+
+    for smile_dir in all_smile_dirs:
+        print("Processing smile_dir: {} {}/{}".format(smile_dir, counter, len(all_smile_dirs)))
+        counter+=1
+        batch_futures[smile_dir] = []
+
+        outdir = "{}/{}".format(args.outdir, os.path.basename(smile_dir))
+        os.makedirs(outdir, exist_ok=True)
+        os.makedirs(outdir + '/logs' , exist_ok=True)
+
+        for pkl_file in os.listdir(smile_dir)[:int(args.num_files)]:
+            if not pkl_file.endswith('.pkl'):
+                continue
+        
+            print("Trying to launch : ", pkl_file)
+            fname = os.path.basename(pkl_file)
+            csv_file = "{}/{}".format(outdir, 
+                                      fname.replace('.pkl', '.csv'))
+            log_file = "{}/logs/{}".format(outdir, 
+                                           fname.replace('.pkl', '.log'))
+
+            pkl_file_path = f"{smile_dir}/{pkl_file}"
+            x = parsl_runner(pkl_file_path,
+                             '/projects/candle_aesp/yadu/Models/scripts/agg_attn.autosave.model.h5', #3CLpro.reg
+                             #'/projects/candle_aesp/yadu/Models/ADRP-P1.reg/agg_attn.autosave.model.h5',
+                             '/projects/candle_aesp/yadu/Models/ADRP-P1.reg/descriptor_headers',
+                             '/projects/candle_aesp/yadu/Models/ADRP-P1.reg/training_headers',
+                             csv_file,
+                             log_file)
+
+            batch_futures[smile_dir].append(x)
+
+        # Waiting for all futures
+        print("Waiting for all futures from {}".format(smile_dir))
+
+        for i in batch_futures[smile_dir]:
+            try:
+                x = i.result()
+
+            except Exception as e:
+                print(f"Exception : {e}")
+                print(f"Chunk {i} failed")
+        print(f"Completed {smile_dir}")    
+
+    print("All done!")
+
