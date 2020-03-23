836e2fb651b0189254f7b22bb37d9019497789b6
Hyunseung "Harry" Yoo <hsyoo@anl.gov>
2020-03-23 17:07:17 -0500

    Estimator (#1)
    * initial version
    
    * initial version
    
    * code cleanup. remove activation for final layer
    
    * fix error on input_size
    
    * parse csv or parquet
    
    * use relu for re-scaled score
    
    * add default value
    
    * add cs1 wrapper
    
    * typo
    
    * remove debug prints
    
    * param key changed to lr_schedule
    
    * reverse default of log_metrics
    
    * activation on the final layer is not supported on cs1
    
    * activate dropout on training mode;
    
    * use lr_schedule
    
    * use pre-scheduled learning rate

---
+++ estimator/to_tfrec.py
@@ -0,0 +1,106 @@
+import argparse
+import os
+from pathlib import Path
+import json
+
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--data_file',
+        type=str,
+        default='adrp-p1.parquet',
+        help='datafile to convert'
+    )
+    parser.add_argument(
+        '--out_dir',
+        default='./data_dir/',
+        help='directory where TFRecords and data info will be stored; '
+             'this directory will be expanded - default is ./data_dir/'
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = get_arguments()
+    scaler = StandardScaler()
+
+    _, ext = os.path.splitext(args.data_file)
+    if ext == '.csv':
+        df = pd.read_csv(args.data_file)
+    elif ext == '.parquet':
+        df = pd.read_parquet(args.data_file)
+    else:
+        print(f'Cannot process {args.data_file}. ext: {ext}')
+        exit()
+
+    PL = df.shape[1] - 1
+    df_y = df.iloc[:, 0].astype(np.float32)
+    df_x = df.iloc[:, 1:(PL + 1)].astype(np.float32)
+    cols = df_x.columns
+    df_x[cols] = scaler.fit_transform(df_x)
+
+    X_train, X_test, Y_train, Y_test = train_test_split(df_x, df_y, test_size=0.20, random_state=42)
+
+    print('x_train shape:', X_train.shape, 'y_train shape:', Y_train.shape)
+    print('x_test shape:', X_test.shape, 'y_test shape:', Y_test.shape)
+
+    if not Path(args.out_dir).is_dir():
+        Path(args.out_dir).mkdir(parents=True)
+
+    summary = {}
+    summary.update(generate_tfrecords(args, 'train', X_train, Y_train))
+    summary.update(generate_tfrecords(args, 'test', X_test, Y_test))
+
+    with open(Path(args.out_dir, 'summary.json'), 'w') as summary_file:
+        summary_file.writelines(json.dumps(summary))
+
+
+def generate_tfrecords(args, partition, features, labels):
+
+    samples_per_batch = 2 ** 13
+    num_examples = len(labels)
+    num_shards = (num_examples + samples_per_batch - 1) // samples_per_batch  # ceiling division
+
+    stat = {f'{partition}_examples': num_examples}
+
+    for shard in range(num_shards):
+        start_index = samples_per_batch * shard
+        end_index = min(samples_per_batch + start_index, num_examples)
+        tfr_path = str(Path(args.out_dir, f'covid.{partition}.{shard:02}.tfrecords'))
+
+        with tf.io.TFRecordWriter(tfr_path) as writer:
+            for i in range(start_index, end_index):
+                feature_dict = {
+                    'score': _float_feature(labels.iloc[i]),
+                    'drug': _float_feature(features.iloc[i].to_list())
+                }
+
+                example = tf.train.Example(features=tf.train.Features(feature=feature_dict))
+                writer.write(example.SerializeToString())
+        print(f'saved {tfr_path}')
+    return stat
+
+
+def _float_feature(value):
+    """
+    Creates tf.Train.Feature from float value.
+    """
+    if value is None:
+        value = []
+    if isinstance(value, np.ndarray) and value.ndim > 1:
+        value = value.reshape(-1)
+    if not isinstance(value, list):
+        value = [value]
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+
+if __name__ == '__main__':
+    main()
---
+++ estimator/train_cs1.py
@@ -0,0 +1,127 @@
+import argparse
+import os
+import functools
+import yaml
+import json
+import math
+from pathlib import Path
+
+import tensorflow as tf
+
+from model import model_fn
+from data import input_fn
+
+from cerebras.tf.cs_estimator import CerebrasEstimator
+from cerebras.tf.run_config import CSRunConfig
+from cerebras.tf.cs_slurm_cluster_resolver import CSSlurmClusterResolver
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cs_ip", default=None, help="CS-1 IP address, defaults to None")
+    parser.add_argument(
+        "--mode",
+        choices=["validate_only", "compile_only", "train", "eval", "predict"],
+        required=True,
+        help=(
+            "Can choose from validate_only, compile_only, train " +
+            "or eval. Defaults to validate_only." +
+            "  Validate only will only go up to kernel matching." +
+            "  Compile only continues through and generate compiled" +
+            "  executables." +
+            "  Train will compile and train if on CS-1," +
+            "  and just train locally (CPU/GPU) if not on CS-1." +
+            "  Eval will run eval locally."
+        ),
+    )
+    parser.add_argument(
+        "--model_dir",
+        default="./model_dir",
+        help="Save outputs")
+    parser.add_argument(
+        "--data_dir",
+        default="./data_dir/top21_auc/",
+        help="Data file location")
+
+    params = vars(parser.parse_args())
+
+    return params
+
+
+def main():
+    params = parse_args()
+
+    with open('model_params.yaml', 'r') as model_file:
+        model_params = yaml.load(model_file)
+
+    # overwrite model_params with command line arguments
+    model_params.update(params)
+
+    # read dataset summary and update model_params
+    with open(Path(params['data_dir'], 'summary.json'), 'r') as summary_file:
+        summary = json.load(summary_file)
+        model_params.update(summary)
+
+    train_steps = math.ceil(model_params['train_examples'] / model_params['batch_size'] / 2.) * 2
+    epoch_steps = train_steps * model_params['epochs']
+    eval_steps = model_params['test_examples'] // model_params['batch_size']
+
+    # sheduled learning rate
+    del model_params['learning_rate']
+    model_params['lr_schedule'] = [(0.001, 0), (0.0001, train_steps * 50), (0.00001, train_steps * 100),
+                                   (0.000001, train_steps * 150), (0.0000001, train_steps * 200)]
+    model_params['xla_compile'] = True
+    print("model_params: ", model_params)
+
+    if params["mode"] == "train":
+        # CS1 configuration
+        use_cs1 = (params["mode"] == "train" and params["cs_ip"] is not None)
+        cs_ip = params["cs_ip"] + ":9000" if use_cs1 else None
+
+        slurm_cluster_resolver = CSSlurmClusterResolver(port_base=23111)
+        cluster_spec = slurm_cluster_resolver.cluster_spec()
+        task_type, task_id = slurm_cluster_resolver.get_task_info()
+        os.environ['TF_CONFIG'] = json.dumps({
+            'cluster': cluster_spec.as_dict(),
+            'task': {
+                'type': task_type,
+                'index': task_id
+            }
+        })
+        config = CSRunConfig(
+            cs_ip=cs_ip,
+            log_step_count_steps=train_steps,
+            save_checkpoints_steps=epoch_steps,
+        )
+        estimator = CerebrasEstimator(
+            model_fn,
+            model_dir=model_params['model_dir'],
+            use_cs=use_cs1,
+            config=config,
+            params=model_params,
+        )
+        print(f'train_steps: {train_steps}, eval_steps: {eval_steps}, total_training_steps: {epoch_steps}')
+        estimator.train(input_fn=functools.partial(input_fn, partition='train'), max_steps=epoch_steps)
+
+    elif params["mode"] == "eval":
+        model_params['log_metrics'] = True
+        estimator = CerebrasEstimator(
+            model_fn,
+            model_dir=model_params['model_dir'],
+            params=model_params,
+        )
+
+        estimator.evaluate(input_fn=functools.partial(input_fn, partition='test'), steps=eval_steps)
+    else:
+        estimator = CerebrasEstimator(
+            model_fn,
+            model_dir=model_params['model_dir'],
+            params=model_params,
+        )
+        estimator.compile(input_fn=functools.partial(input_fn, params=model_params, partition='test'),
+                          validate_only=(params["mode"] == "validate_only"))
+
+if __name__ == '__main__':
+    main()
