ebb970a75736195c6d5956d16dfa427e1ed48aed
Yadu Nand B <yadudoc1729@gmail.com>
2020-03-27 17:18:43 +0000

    U

--- scripts/run_all_models.sh
+++ scripts/run_all_models.sh
@@ -9,32 +9,8 @@ models=($(cat models_to_run.txt))
 for i in ${models[@]}
 do 
     echo "STARTING ##########################################################################################"
-    modelname=$(basename $i)
-    if [[ $modelname == "3CLpro.reg" ]]
-    then
-	echo "Skipping $modelname"
-	continue
-    fi
-    if [[ $modelname == "ADRP-P13.bin" ]]
-    then
-	echo "Skipping $modelname"
-	continue
-    fi
-    
-    modelpath=($(ls -rt /projects/CVD_Research/BoxMirror/drug-screening/ML-models/$i/*autosave.model.h5))
-    if [[ ${#modelpath[@]} == 0 ]]
-    then
-	echo "WARNING: $modelname has multiple conflicting models. Skipping"
-	echo "${modelpath[*]}"
-	continue
-    fi
-
-    if [[ ${#modelpath[@]} > 1 ]]
-    then
-	echo "Multiple : ${modelpath[@]}"
-	modelpath=${modelpath[-1]}
-	echo "Modelpath set to ${modelpath[0]}"
-    fi
+    modelname=$(basename $(dirname $i))    
+    modelpath=$i
 
     echo "Starting processing of $modelname at $(date)"
     echo "Running $modelname with path : $modelpath"
@@ -42,6 +18,7 @@ do
     if [[ "$debug" == "skip" ]]
     then
 	echo "Debug skip"
+
     elif [[ "$debug" == "debug" ]]
     then
 	python3 test.py -s '/projects/candle_aesp/Descriptors/Enamine_Real/*' \
@@ -51,15 +28,15 @@ do
 	    -c $config
     elif [[ "$debug" == "prod" ]]
     then
-	python3 test.py -s '/projects/candle_aesp/Descriptors/Enamine_Real/*' \
+	python3 test.py -s '/projects/CVD_Research/datasets/Enamine_Real/*' \
 	    -o $PWD/Enamine_Infer_$modelname \
 	    -m ${modelpath[0]} \
             -n 10000  \
 	    -c theta
+	mail -s "[Theta] $modelname completed with $status" yadudoc1729@gmail.com < /dev/null
     fi
     status=$?
     echo "Completed -------------------------- $(date)"
-    # mail -s "[Theta] $modelname completed with $status" yadudoc1729@gmail.com < /dev/null
 
     echo -e "\n"
     echo "COMPLETED ##########################################################################################"
