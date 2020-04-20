6a1dc3056ee4db9b37dd0b38b92b0ec402d5067b
Yadu Nand B <yadudoc1729@gmail.com>
2020-04-20 22:24:07 +0000

    Minor changes

--- scripts/run_all_models.sh
+++ scripts/run_all_models.sh
@@ -1,46 +1,60 @@
 #!/bin/bash
-#set -x
+set -x
 
 debug=$1
 config=$2
 
-models=($(cat models_to_run.txt))
-# for i in $(ls /projects/CVD_Research/BoxMirror/drug-screening/ML-models/)
-for i in ${models[@]}
-do 
-    echo "STARTING ##########################################################################################"
-    modelname=$(basename $(dirname $i))    
-    modelpath=$i
+run() {
+    
+    data_source="$1"
+    output_prefix="$2"
+    mkdir -p $output_prefix
 
-    echo "Starting processing of $modelname at $(date)"
-    echo "Running $modelname with path : $modelpath"
+    models=($(cat models_to_run.txt))
+    # for i in $(ls /projects/CVD_Research/BoxMirror/drug-screening/ML-models/)
+    for i in ${models[@]}
+    do 
+	echo "STARTING ##########################################################################################"
+	modelname=$(basename $(dirname $i))    
+	modelpath=$i
 
-    if [[ "$debug" == "skip" ]]
-    then
-	echo "Debug skip"
+	echo "Starting processing of $modelname at $(date)"
+	echo "Running $modelname with path : $modelpath"
 
-    elif [[ "$debug" == "debug" ]]
-    then
-	python3 test.py -s '/projects/candle_aesp/Descriptors/Enamine_Real/*' \
-	    -o $PWD/Enamine_Infer_$modelname \
-	    -m ${modelpath[0]} \
-            -n 10  \
-	    -c $config
-    elif [[ "$debug" == "prod" ]]
-    then
-	python3 test.py -s '/projects/CVD_Research/datasets/Enamine_Real/*' \
-	    -o $PWD/Enamine_Infer_$modelname \
-	    -m ${modelpath[0]} \
-            -n 10000  \
-	    -c theta
-	mail -s "[Theta] $modelname completed with $status" yadudoc1729@gmail.com < /dev/null
-    fi
-    status=$?
-    echo "Completed -------------------------- $(date)"
+	if [[ "$debug" == "skip" ]]
+	then
+	    echo "Debug skip"
 
-    echo -e "\n"
-    echo "COMPLETED ##########################################################################################"
-done
+	elif [[ "$debug" == "debug" ]]
+	then
+	    python3 test.py -s "$data_source" \
+		-o $PWD/$output_prefix/$output_prefix_$modelname \
+		-m ${modelpath[0]} \
+		-n 10  \
+		-c $config
+	elif [[ "$debug" == "prod" ]]
+	then
+	    python3 test.py -s "$data_source" \
+		-o $PWD/$output_prefix/$output_prefix_$modelname \
+		-m ${modelpath[0]} \
+		-n 10000  \
+		-c theta
+	    mail -s "[Theta] $modelname on $output_prefix completed with $status" yadudoc1729@gmail.com < /dev/null
+	fi
+	status=$?
+	echo "Completed -------------------------- $(date)"
 
+	echo -e "\n"
+	echo "COMPLETED ##########################################################################################"
+    done
+
+    mail -s "[Theta] All inference completed for $output_prefix" yadudoc1729@gmail.com < /dev/null
+}
+
+
+#run '/projects/CVD_Research/datasets/Zinc15_descriptors/*' "Z15_Infer"
+run '/projects/CVD_Research/datasets/descriptors-all/savi_descriptors/*' "SAV_Infer"
+run '/projects/CVD_Research/datasets/descriptors-all/pubchem128_descriptors/*' "PCH_Infer"
+run '/projects/CVD_Research/datasets/descriptors-all/GDB-17_descriptors/*' "G17_Infer"
 
 
