2e95a9fba0e6e822d9eb4b223178844be471e7a9
Yadu Nand B <yadudoc1729@gmail.com>
2020-04-29 05:02:07 +0000

    Specify output paths to bash fn.

--- scripts/run_all_models.sh
+++ scripts/run_all_models.sh
@@ -4,10 +4,13 @@ set -x
 debug=$1
 config=$2
 
+
 run() {
     
     data_source="$1"
     output_prefix="$2"
+    outdir="$3"
+
     mkdir -p $output_prefix
 
     if [[ "$debug" == "skip" ]]
@@ -17,14 +20,14 @@ run() {
     elif [[ "$debug" == "debug" ]]
     then
 	python3 driver.py -s "$data_source" \
-	    -o $PWD/$output_prefix/$output_prefix \
+	    -o $outdir/$output_prefix/$output_prefix \
 	    -m models_to_run.txt \
 	    -n 10  \
 	    -c $config	
     elif [[ "$debug" == "prod" ]]
     then
 	python3 driver.py -s "$data_source" \
-	    -o $PWD/$output_prefix/$output_prefix \
+	    -o $outdir/$output_prefix/$output_prefix \
 	    -m models_to_run.txt \
 	    -n 10000  \
 	    -c $config
@@ -43,7 +46,7 @@ run() {
 #run '/projects/CVD_Research/datasets/Zinc15_descriptors/*' "Z15_Infer"
 #run '/gpfs/alpine/med110/scratch/yadunan/descriptors/SAV/*' "SAV_Infer"
 #run '/gpfs/alpine/med110/scratch/yadunan/descriptors/ZIN/ZIN*' "ZIN_Infer"
-run '/projects/CVD_Research/datasets/release/descriptors/ENA/*' "ENA_Infer"
+run '/projects/CVD_Research/datasets/release/descriptors/ENA/*' "ENA_Infer" '/projects/CVD_Research/datasets/Infer_V5_April25'
 
 
 
